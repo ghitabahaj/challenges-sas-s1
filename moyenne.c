f7126370f17a05b14810da6e8ab1a13f7ac6935d
bahaj <bahajghita3@gmail.com>
2022-09-08 13:48:56 +0100

    5th commit today

---
+++ moyenne.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+
+
+
+int main() {
+     float moyenne = 0;
+     printf("BONJOUR!");
+     printf ("veuillez entrer la moyenne de l eleve :");
+     scanf("%f",&moyenne);
+     
+     
+     if(moyenne < 10 ){
+     	printf("l eleve est recale !");
+	 }else if(moyenne >=10 && moyenne < 12){
+	 	printf("la mention de cet eleve est : PASSABLE!");
+	 }else if (moyenne >=12 && moyenne < 14	 ){
+	 	printf("la mention de cet eleve est : ASSEZ BIEN !");
+	 }else if (moyenne >=14 && moyenne < 16	 ){
+	 	printf("la mention de cet eleve est : BIEN !");
+	 }else{
+	 	printf ("la mention de cet eleve est :  TRES BIEN !");
+	 }
+	
+	
+	return 0;
+}
